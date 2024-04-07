0c9d4daad29d9a3bdf6400214e443dba727f1ecb
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-04-07 14:31:04 +0300

    color dict for every tile, https://github.com/daniel-huang-1230/Game-2048/blob/master/Gui2048.java Values are taken from this link.

---
+++ Tetris_2048/tile_color.py
@@ -0,0 +1,48 @@
+from lib.color import Color
+
+tile_colors = {
+    2: {
+        "background_color": Color(238, 228, 218),
+        "foreground_color": Color(138, 129, 120)
+    },
+    4: {
+        "background_color": Color(237, 224, 200),
+        "foreground_color": Color(138, 129, 120)
+    },
+    8: {
+        "background_color": Color(242, 177, 121),
+        "foreground_color": Color(255, 255, 255)
+    },
+    16: {
+        "background_color": Color(245, 149, 99),
+        "foreground_color": Color(255, 255, 255)
+    },
+    32: {
+        "background_color": Color(246, 124, 95),
+        "foreground_color": Color(255, 255, 255)
+    },
+    64: {
+        "background_color": Color(246, 94, 59),
+        "foreground_color": Color(255, 255, 255)
+    },
+    128: {
+        "background_color": Color(237, 207, 114),
+        "foreground_color": Color(255, 255, 255)
+    },
+    256: {
+        "background_color": Color(237, 204, 97),
+        "foreground_color": Color(255, 255, 255)
+    },
+    512: {
+        "background_color": Color(237, 200, 80),
+        "foreground_color": Color(255, 255, 255)
+    },
+    1024: {
+        "background_color": Color(237, 197, 63),
+        "foreground_color": Color(255, 255, 255)
+    },
+    2048: {
+        "background_color": Color(237, 197, 46),
+        "foreground_color": Color(255, 255, 255)
+    }
+}
