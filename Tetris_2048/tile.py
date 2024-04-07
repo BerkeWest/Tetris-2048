8754a72e983ba6a64fe1e9d0124c4571061fe73f
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-04-08 02:47:42 +0300

    merging tiles, and checking for merge if exist.

--- Tetris_2048/tile.py
+++ Tetris_2048/tile.py
@@ -1,4 +1,7 @@
 import random
+
+import numpy as np
+
 from tile_color import tile_colors
 
 import lib.stddraw as stddraw  # stddraw is used as a basic graphics library
@@ -28,6 +31,33 @@ class Tile:
     def update_color(self):
         self.background_color = tile_colors[self.number]['background_color']
         self.foreground_color = tile_colors[self.number]['foreground_color']
+
+    def merge_matches(self, tile):
+        if self.number == tile.number and self.number < 2048:
+            self.number *= 2
+            tile.number = None
+            self.update_color()
+            return self.number
+        return 0
+
+    @staticmethod
+    def merge_tiles(tile_matrix, score):
+        for row, col in np.ndindex(tile_matrix.shape):
+            current_tile = tile_matrix[row, col]
+            if current_tile is not None:
+                if row < tile_matrix.shape[0] - 1 and tile_matrix[row + 1, col] is not None:
+                    adjacent_tile = tile_matrix[row + 1, col]
+                    if current_tile.number == adjacent_tile.number:
+                        score += current_tile.merge_matches(adjacent_tile)
+                        tile_matrix[row + 1, col] = None
+                        for r in range(row + 1, tile_matrix.shape[0]):
+                            if tile_matrix[r, col] is not None:
+                                tile_matrix[r - 1, col] = tile_matrix[r, col]
+                                tile_matrix[r, col] = None
+                if row > 0 and tile_matrix[row - 1, col] is None and np.all(tile_matrix[:row, col] == None):
+                    tile_matrix[row - 1, col] = current_tile
+                    tile_matrix[row, col] = None
+        return score
 
     # Method for drawing the tile
     def draw(self, position, length=1):
